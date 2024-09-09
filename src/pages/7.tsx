b050a9d42291cdcddf7b89a060fe554364d269b0
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2024-09-09 06:49:42 +0530

    Update 7.tsx

--- src/pages/7.tsx
+++ src/pages/7.tsx
@@ -446,6 +446,7 @@ export default function Fifth_SP() {
           
         
         </div>
+<p>G2 Licensed Agent : Jessica Johanna Rodriguez</p>
         {/* <p>{zipCode} </p> */}
       </div>
       {/* <ToastContainer
